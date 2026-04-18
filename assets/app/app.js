06e03582b9e823c387a53888763b0bf861c99ee4
rahmatbrylyant975@gmail.com <rahmatbrylyant975@gmail.com>
2026-04-18 22:31:53 +0700

    plus and minus btn

---
+++ assets/app/app.js
@@ -0,0 +1,30 @@
+// FLOW SISTEM
+// User klik + / - → update qty
+// Klik submit → ambil semua data
+// Filter item yang qty > 0
+// Susun jadi text
+// Encode → kirim ke WhatsApp
+
+// Ambil semua products card 
+const card = document.querySelectorAll(".products__card");
+
+//=======================
+// 1. HANDLE QTY ( +/- )
+// =======================
+
+cards.forEach((card) => {
+    const plusBtn = card.querySelector(".qty__plus");
+    const minusBtn = card.querySelector(".qty__minus");
+    const qtyEl = card.querySelector(".qty__num");
+    
+    let qty = 0;
+
+    plusBtn.addEventListener("click", ()=>{
+        qty++;
+        
+    })
+}
+
+
+
+)
